d26fbe76334e0b80c50f8bee708b15f551608552
hesha <example@gmail.com>
2022-07-19 20:44:33 +0100

    other changes pre deployment

--- ToDo/App/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import List, ListItem, Note
-# Register your models here.
-
-admin.site.register(List)
-admin.site.register(ListItem)
-admin.site.register(Note)
--- ToDo/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ApiList, ApiNote, ApiListItem, ApiCategory
-# Register your models here.
-
-admin.site.register(ApiList)
-admin.site.register(ApiListItem)
-admin.site.register(ApiNote)
-admin.site.register(ApiCategory)
