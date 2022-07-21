617067a93b26a816abff75a633c60d36770d8c31
hesha <example@gmail.com>
2022-07-21 19:03:44 +0100

    change name to thoughtapp3

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'thoughtapp.apps.AppConfig',
+    # 'thoughtapp.apps.AppConfig',
     'api.apps.ApiConfig',
     "corsheaders",
 ]
